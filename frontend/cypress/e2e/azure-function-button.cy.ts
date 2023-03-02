12fa58b21408f836ee503e7410889aa5bf16563b
Johan Günther Krogevoll <johan@krogevoll.com>
2023-03-02 19:55:41 +0100

    wait

--- frontend/cypress/e2e/azure-function-button.cy.ts
+++ frontend/cypress/e2e/azure-function-button.cy.ts
@@ -2,8 +2,9 @@ export { }
 
 describe('Use name in greeting from azure function if logged in.', () => {
   it('passes', () => {
-    cy.intercept('GET', '/.auth/me', { fixture: 'easyauth-me.json' })
+    cy.intercept('GET', '/.auth/me', { fixture: 'easyauth-me.json' }).as('auth')
     cy.visit('/')
+    cy.wait('@auth')
     cy.contains('Say hi to Azure functions').click()
     cy.dataCy('greeting').should('contain', 'Hello Test Testesen! This is a message from Azure functions.')
   })
